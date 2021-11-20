58bb06275f36b0d11f5aca90e32d344bb577395a
GerasimenkoOksana <69861052+GerasimenkoOksana@users.noreply.github.com>
2021-11-20 17:45:31 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function slider(activeSlide=0){
+    const slides = document.querySelectorAll('.slide')
+    slides[activeSlide].classList.add('active')
+    for (const slide of slides) {
+        slide.addEventListener('click', () =>{
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
+    }
+    function clearActiveClasses() {
+        slides.forEach((slide) =>{
+            slide.classList.remove('active')
+        })
+    }
+}
+const index = Math.floor(Math.random()*5)
+slider(index);
